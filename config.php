77da0d49e18705c0f7c12e2f1f928f7fd43a709e
NQ <121035503+neiruhitori@users.noreply.github.com>
2024-10-09 13:32:17 +0700

    penyesuaian config

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ date_default_timezone_set("Asia/Jakarta");
 error_reporting(0);
 
 	// sesuaikan dengan server anda
-	$host 	= 'localhost'; // host server
+	$host 	= 'localhost:3309 || localhost'; // host server
 	$user 	= 'root';  // username server
 	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
 	$dbname = 'db_toko'; // nama database anda
