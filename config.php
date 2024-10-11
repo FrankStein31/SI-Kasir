a63675d14dbf1a5328d711f0a248953604199fd4
FrankStein31 <frankie.intern24slides@gmail.com>
2024-10-11 14:40:52 +0700

    Config 2 host

--- config.php
+++ config.php
@@ -4,10 +4,7 @@ error_reporting(0);
 
 	// sesuaikan dengan server anda
 	$host 	= 'localhost'; // host server
-<<<<<<< HEAD
 	$host2 	= 'localhost:3309'; // host server
-=======
->>>>>>> 760f0b15c1f4e4c8ee43238aa21e4e8306c89772
 	$user 	= 'root';  // username server
 	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
 	$dbname = 'db_toko'; // nama database anda
@@ -16,6 +13,7 @@ error_reporting(0);
 		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
 		//echo 'sukses';
 	}catch(PDOException $e){
+		$config = new PDO("mysql:host=$host2;dbname=$dbname;", $user,$pass);
 		echo 'KONEKSI GAGAL' .$e -> getMessage();
 	}
 	
