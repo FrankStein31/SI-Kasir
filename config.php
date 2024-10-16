839f167b5c2e8a55a95880ce6c0e802f38e1784a
NQ <121035503+neiruhitori@users.noreply.github.com>
2024-10-16 11:10:43 +0700

    ...

--- config.php
+++ config.php
@@ -4,7 +4,6 @@ error_reporting(0);
 
 	// sesuaikan dengan server anda
 	$host 	= 'localhost'; // host server
-	$host2 	= 'localhost:3309'; // host server
 	$user 	= 'root';  // username server
 	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
 	$dbname = 'db_toko'; // nama database anda
@@ -13,8 +12,7 @@ error_reporting(0);
 		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
 		//echo 'sukses';
 	}catch(PDOException $e){
-		$config = new PDO("mysql:host=$host2;dbname=$dbname;", $user,$pass);
-		// echo 'KONEKSI GAGAL' .$e -> getMessage();
+		echo 'KONEKSI GAGAL' .$e -> getMessage();
 	}
 	
 	$view = 'fungsi/view/view.php'; // direktori fungsi select data
