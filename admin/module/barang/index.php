0a6cd552ecf1c14cf7b77a2f98e895de722fde40
Eka Yulianita Widyanti <eywidyanti@gmail.com>
2024-12-17 12:39:39 +0700

    eka

--- admin/module/barang/index.php
+++ admin/module/barang/index.php
@@ -1,40 +1,58 @@
-        <h4>Data Barang</h4>
+<h4>Data Barang</h4>
         <br />
-        <?php if(isset($_GET['success-stok'])){?>
-        <div class="alert alert-success">
-            <p>Tambah Stok Berhasil !</p>
-        </div>
-        <?php }?>
-        <?php if(isset($_GET['success'])){?>
-        <div class="alert alert-success">
-            <p>Tambah Data Berhasil !</p>
-        </div>
-        <?php }?>
-        <?php if(isset($_GET['remove'])){?>
-        <div class="alert alert-danger">
-            <p>Hapus Data Berhasil !</p>
-        </div>
-        <?php }?>
+        <?php if (isset($_GET['success-stok'])) { ?>
+            <div class="alert alert-success">
+                <p>Tambah Stok Berhasil !</p>
+            </div>
+        <?php } ?>
+        <?php if (isset($_GET['success'])) { ?>
+            <div class="alert alert-success">
+                <p>Tambah Data Berhasil !</p>
+            </div>
+        <?php } ?>
+        <?php if (isset($_GET['remove'])) { ?>
+            <div class="alert alert-danger">
+                <p>Hapus Data Berhasil !</p>
+            </div>
+        <?php } ?>
 
-        <?php 
-			$sql=" select * from barang where stok <= 3";
-			$row = $config -> prepare($sql);
-			$row -> execute();
-			$r = $row -> rowCount();
-			if($r > 0){
-				echo "
+        <?php
+        $sql = " select * from barang where stok <= 3";
+        $row = $config->prepare($sql);
+        $row->execute();
+        $r = $row->rowCount();
+        if ($r > 0) {
+            echo "
 				<div class='alert alert-warning'>
 					<span class='glyphicon glyphicon-info-sign'></span> Ada <span style='color:red'>$r</span> barang yang Stok tersisa sudah kurang dari 3 items. silahkan pesan lagi !!
 					<span class='pull-right'><a href='index.php?page=barang&stok=yes'>Cek Barang <i class='fa fa-angle-double-right'></i></a></span>
 				</div>
-				";	
-			}
-		?>
+				";
+        }
+
+        $sql1 = "SELECT * FROM barang WHERE DATEDIFF(expired, CURDATE()) <= 7";
+        $row1 = $config->prepare($sql1);
+        $row1->execute();
+        $r1 = $row1->rowCount();
+
+        if ($r1 > 0) {
+            echo "
+                <div class='alert alert-warning'>
+                    <span class='glyphicon glyphicon-info-sign'></span> Ada <span style='color:red'>$r1</span> barang yang akan kedaluwarsa dalam waktu kurang dari 7 hari. Silakan cek dan lakukan tindakan yang diperlukan!
+                    <span class='pull-right'><a href='index.php?page=barang&expired=yes'>Cek Barang <i class='fa fa-angle-double-right'></i></a></span>
+                </div>
+                ";
+        }
+
+        ?>
         <!-- Trigger the modal with a button -->
         <button type="button" class="btn btn-primary btn-md mr-2" data-toggle="modal" data-target="#myModal">
             <i class="fa fa-plus"></i> Tambah Data</button>
         <a href="index.php?page=barang&stok=yes" class="btn btn-warning btn-md mr-2">
             <i class="fa fa-list"></i> Sortir Stok Kurang</a>
+        <a href="index.php?page=barang&expired=yes" class="btn btn-warning btn-md mr-2">
+            <i class="fa fa-list"></i> Sortir Kedaluwarsa
+        </a>
         <a href="index.php?page=barang" class="btn btn-success btn-md">
             <i class="fa fa-refresh"></i> Refresh Data</a>
         <div class="clearfix"></div>
@@ -59,75 +77,77 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php 
-						$totalBeli = 0;
-						$totalJual = 0;
-						$totalStok = 0;
-						if($_GET['stok'] == 'yes')
-						{
-							$hasil = $lihat -> barang_stok();
+                        <?php
+                        $totalBeli = 0;
+                        $totalJual = 0;
+                        $totalStok = 0;
+                        
+                        if ($_GET['stok'] == 'yes') {
+                            $hasil = $lihat->barang_stok();
+                        } elseif ($_GET['expired'] == 'yes') {
+                            $hasil = $lihat->barang_expired();
+                        } else {
+                            $hasil = $lihat->barang();
+                        }
 
-						}else{
-							$hasil = $lihat -> barang();
-						}
-						$no=1;
-						foreach($hasil as $isi) {
-					?>
-                        <tr>
-                            <td><?php echo $no;?></td>
-                            <td><?php echo $isi['id_barang'];?></td>
-                            <td><?php echo $isi['nama_kategori'];?></td>
-                            <td><?php echo $isi['nama_barang'];?></td>
-                            <td><?php echo $isi['merk'];?></td>
-                            <td>
-                                <?php if($isi['stok'] == '0'){?>
-                                <button class="btn btn-danger"> Habis</button>
-                                <?php }else{?>
-                                <?php echo $isi['stok'];?>
-                                <?php }?>
-                            </td>
-                            <td>Rp.<?php echo number_format($isi['harga_beli']);?>,-</td>
-                            <td>Rp.<?php echo number_format($isi['harga_jual']);?>,-</td>
-                            <td> <?php echo $isi['satuan_barang'];?></td>
-                            <td><?php echo $isi['expired'];?></td>
-                            <td>
-                                <?php if($isi['stok'] <=  '3'){?>
-                                <form method="POST" action="fungsi/edit/edit.php?stok=edit">
-                                    <input type="text" name="restok" class="form-control">
-                                    <input type="hidden" name="id" value="<?php echo $isi['id_barang'];?>"
-                                        class="form-control">
-                                    <button class="btn btn-primary btn-sm">
-                                        Restok
-                                    </button>
-                                    <a href="fungsi/hapus/hapus.php?barang=hapus&id=<?php echo $isi['id_barang'];?>"
-                                        onclick="javascript:return confirm('Hapus Data barang ?');">
-                                        <button class="btn btn-danger btn-sm">Hapus</button></a>
-                                </form>
-                                <?php }else{?>
-                                <a href="index.php?page=barang/details&barang=<?php echo $isi['id_barang'];?>"><button
-                                        class="btn btn-primary btn-xs">Details</button></a>
+                        $no = 1;
+                        foreach ($hasil as $isi) {
+                        ?>
+                            <tr>
+                                <td><?php echo $no; ?></td>
+                                <td><?php echo $isi['id_barang']; ?></td>
+                                <td><?php echo $isi['nama_kategori']; ?></td>
+                                <td><?php echo $isi['nama_barang']; ?></td>
+                                <td><?php echo $isi['merk']; ?></td>
+                                <td>
+                                    <?php if ($isi['stok'] == '0') { ?>
+                                        <button class="btn btn-danger"> Habis</button>
+                                    <?php } else { ?>
+                                        <?php echo $isi['stok']; ?>
+                                    <?php } ?>
+                                </td>
+                                <td>Rp.<?php echo number_format($isi['harga_beli']); ?>,-</td>
+                                <td>Rp.<?php echo number_format($isi['harga_jual']); ?>,-</td>
+                                <td> <?php echo $isi['satuan_barang']; ?></td>
+                                <td><?php echo date('j F Y', strtotime($isi['expired'])); ?></td>
+                                <td>
+                                    <?php if ($isi['stok'] <=  '3') { ?>
+                                        <form method="POST" action="fungsi/edit/edit.php?stok=edit">
+                                            <input type="text" name="restok" class="form-control">
+                                            <input type="hidden" name="id" value="<?php echo $isi['id_barang']; ?>"
+                                                class="form-control">
+                                            <button class="btn btn-primary btn-sm">
+                                                Restok
+                                            </button>
+                                            <a href="fungsi/hapus/hapus.php?barang=hapus&id=<?php echo $isi['id_barang']; ?>"
+                                                onclick="javascript:return confirm('Hapus Data barang ?');">
+                                                <button class="btn btn-danger btn-sm">Hapus</button></a>
+                                        </form>
+                                    <?php } else { ?>
+                                        <a href="index.php?page=barang/details&barang=<?php echo $isi['id_barang']; ?>"><button
+                                                class="btn btn-primary btn-xs">Details</button></a>
 
-                                <a href="index.php?page=barang/edit&barang=<?php echo $isi['id_barang'];?>"><button
-                                        class="btn btn-warning btn-xs">Edit</button></a>
-                                <a href="fungsi/hapus/hapus.php?barang=hapus&id=<?php echo $isi['id_barang'];?>"
-                                    onclick="javascript:return confirm('Hapus Data barang ?');"><button
-                                        class="btn btn-danger btn-xs">Hapus</button></a>
-                                <?php }?>
-                        </tr>
-                        <?php 
-							$no++; 
-							$totalBeli += $isi['harga_beli'] * $isi['stok']; 
-							$totalJual += $isi['harga_jual'] * $isi['stok'];
-							$totalStok += $isi['stok'];
-						}
-					?>
+                                        <a href="index.php?page=barang/edit&barang=<?php echo $isi['id_barang']; ?>"><button
+                                                class="btn btn-warning btn-xs">Edit</button></a>
+                                        <a href="fungsi/hapus/hapus.php?barang=hapus&id=<?php echo $isi['id_barang']; ?>"
+                                            onclick="javascript:return confirm('Hapus Data barang ?');"><button
+                                                class="btn btn-danger btn-xs">Hapus</button></a>
+                                    <?php } ?>
+                            </tr>
+                        <?php
+                            $no++;
+                            $totalBeli += $isi['harga_beli'] * $isi['stok'];
+                            $totalJual += $isi['harga_jual'] * $isi['stok'];
+                            $totalStok += $isi['stok'];
+                        }
+                        ?>
                     </tbody>
                     <tfoot>
                         <tr>
                             <th colspan="5">Total </td>
-                            <th><?php echo $totalStok;?></td>
-                            <th>Rp.<?php echo number_format($totalBeli);?>,-</td>
-                            <th>Rp.<?php echo number_format($totalJual);?>,-</td>
+                            <th><?php echo $totalStok; ?></td>
+                            <th>Rp.<?php echo number_format($totalBeli); ?>,-</td>
+                            <th>Rp.<?php echo number_format($totalJual); ?>,-</td>
                             <th colspan="2" style="background:#ddd"></th>
                         </tr>
                     </tfoot>
@@ -150,11 +170,11 @@
                         <div class="modal-body">
                             <table class="table table-striped bordered">
                                 <?php
-									$format = $lihat -> barang_id();
-								?>
+                                $format = $lihat->barang_id();
+                                ?>
                                 <tr>
                                     <td>ID Produk</td>
-                                    <td><input type="text" readonly="readonly" required value="<?php echo $format;?>"
+                                    <td><input type="text" readonly="readonly" required value="<?php echo $format; ?>"
                                             class="form-control" name="id"></td>
                                 </tr>
                                 <tr>
@@ -162,10 +182,11 @@
                                     <td>
                                         <select name="kategori" class="form-control" required>
                                             <option value="#">Pilih Kategori</option>
-                                            <?php  $kat = $lihat -> kategori(); foreach($kat as $isi){ 	?>
-                                            <option value="<?php echo $isi['id_kategori'];?>">
-                                                <?php echo $isi['nama_kategori'];?></option>
-                                            <?php }?>
+                                            <?php $kat = $lihat->kategori();
+                                            foreach ($kat as $isi) {     ?>
+                                                <option value="<?php echo $isi['id_kategori']; ?>">
+                                                    <?php echo $isi['nama_kategori']; ?></option>
+                                            <?php } ?>
                                         </select>
                                     </td>
                                 </tr>
@@ -212,7 +233,7 @@
                                 <tr>
                                     <td>Tanggal Input</td>
                                     <td><input type="text" required readonly="readonly" class="form-control"
-                                            value="<?php echo  date("j F Y, G:i");?>" name="tgl"></td>
+                                            value="<?php echo  date("j F Y, G:i"); ?>" name="tgl"></td>
                                 </tr>
                             </table>
                         </div>
